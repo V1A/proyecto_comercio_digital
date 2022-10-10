228daa4f074c4f0cf847a7032315deb04075a014
Via Vianca Contreras Colque <vi.122494@gmail.com>
2022-10-09 20:52:24 -0400

    Measure Unit view set implemented

--- products/api/views/general_views.py
+++ products/api/views/general_views.py
@@ -1,5 +1,15 @@
+from rest_framework import viewsets
+from rest_framework.parsers import JSONParser, MultiPartParser
+
 from base.api import GeneralListApiView
 from products.api.serializers.general_serializers import MeasureUnitSerializer, IndicatorSerializer, CategoryProductSerializer
+
+class MeasureUnitViewSet(viewsets.ModelViewSet):
+    serializer_class = MeasureUnitSerializer
+    parser_classes = (JSONParser, MultiPartParser, )
+
+    def get_queryset(self):
+        return self.get_serializer().Meta.model.objects.filter(state=True)
 
 class MeasureUnitListAPIView(GeneralListApiView):
     serializer_class = MeasureUnitSerializer
--- products/api/views/product_viewsets.py
+++ products/api/views/product_viewsets.py
@@ -4,7 +4,6 @@ from rest_framework.response import Response
 from rest_framework.parsers import JSONParser, MultiPartParser
 from rest_framework import viewsets
 
-from base.api import GeneralListApiView
 from products.api.serializers.product_serializers import ProductSerializer
 
 class ProductViewSet(viewsets.ModelViewSet):
